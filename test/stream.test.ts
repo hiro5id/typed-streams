8dbf16413e461c0eef4e7bbec59ebd5fe355cc65
Roberto Sebestyen <roberto@sebestyen.ca>
2019-12-20 06:24:18 -0500

    update test

--- test/stream.test.ts
+++ test/stream.test.ts
@@ -2,143 +2,6 @@
 import { Readable, Transform } from '../src';
 
 describe('stream', () => {
-
-
-  interface INewCar {
-    newCarNumber: number;
-  }
-
-  interface IUsedCar {
-    usedCarNumber: number;
-  }
-
-  interface IStolenCar {
-    stolenCarNumber: number;
-  }
-
-  class NewCarFactory extends Readable<INewCar> {
-    private i = 0;
-
-    constructor() {
-      super({ objectMode: true });
-    }
-
-    public _read(): any {
-      process.nextTick(() => {
-        const v = this.i++ < 20 ? { newCarNumber: this.i } as INewCar : null;
-        this.push(v);
-        if (v) {
-          console.log('produced car# ', v.newCarNumber);
-        }
-      });
-    }
-  }
-
-  class NewCarBuyer extends Transform<INewCar, IUsedCar[]> {
-    private newCars: INewCar[] = [];
-
-    constructor(private readonly numberOfCarsToBuyAtATime: number) {
-      super({ objectMode: true });
-    }
-
-    public _flush(callback: (error?: (Error | null), data?: any) => void): void {
-      process.nextTick(() => {
-        if (this.newCars.length) {
-          this.push(this.newCars.map(m => ({ usedCarNumber: m.newCarNumber } as IUsedCar)));
-          console.log(`purchased ${this.newCars.length} car numbers: ${this.newCars.map(m => m.newCarNumber).join(',')}`);
-        }
-        this.newCars = [];
-        callback();
-      });
-    }
-
-    // tslint:disable-next-line:variable-name
-    public _transform(chunk: INewCar, _encoding: string, callback: (error?: (Error | null), data?: any) => void): void {
-      process.nextTick(() => {
-        this.newCars.push(chunk);
-        console.log('shopping for NEW car# ', chunk.newCarNumber);
-        if (this.newCars.length >= this.numberOfCarsToBuyAtATime) {
-          return this._flush(callback);
-        }
-        callback();
-      });
-    }
-  }
-
-  class UsedCarBuyer extends Transform<IUsedCar[], IUsedCar[]> {
-    private newCars: IUsedCar[] = [];
-
-    constructor(private readonly numberOfCarsToBuyAtATime: number) {
-      super({ objectMode: true });
-    }
-
-    public _flush(callback: (error?: (Error | null), data?: any) => void): void {
-      process.nextTick(() => {
-        if (this.newCars.length) {
-          this.push(this.newCars.map(m => ({ usedCarNumber: m.usedCarNumber } as IUsedCar)));
-          console.log(`purchased used ${this.newCars.length} car numbers: ${this.newCars.map(m => m.usedCarNumber).join(',')}`);
-        }
-        this.newCars = [];
-        callback();
-      });
-    }
-
-    // tslint:disable-next-line:variable-name
-    public _transform(chunk: IUsedCar[], _encoding: string, callback: (error?: (Error | null), data?: any) => void): void {
-      process.nextTick(() => {
-        chunk.forEach(c => this.newCars.push(c));
-        console.log('shopping for USED car# ', chunk.map(m => m.usedCarNumber).join(","));
-        if (this.newCars.length >= this.numberOfCarsToBuyAtATime) {
-          return this._flush(callback);
-        }
-        callback();
-      });
-    }
-  }
-
-  class StolenCarBuyer extends Transform<IStolenCar, IUsedCar[]> {
-    private newCars: IStolenCar[] = [];
-
-    constructor(private readonly numberOfCarsToBuyAtATime: number) {
-      super({ objectMode: true });
-    }
-
-    public _flush(callback: (error?: (Error | null), data?: any) => void): void {
-      process.nextTick(() => {
-        if (this.newCars.length) {
-          this.push(this.newCars.map(m => ({ usedCarNumber: m.stolenCarNumber } as IUsedCar)));
-          console.log(`purchased STOLEN ${this.newCars.length} car numbers: ${this.newCars.map(m => m.stolenCarNumber).join(',')}`);
-        }
-        this.newCars = [];
-        callback();
-      });
-    }
-
-    // tslint:disable-next-line:variable-name
-    public _transform(chunk: IStolenCar, _encoding: string, callback: (error?: (Error | null), data?: any) => void): void {
-      process.nextTick(() => {
-        this.newCars.push(chunk);
-        console.log('shopping for stolen car# ', chunk.stolenCarNumber);
-        if (this.newCars.length >= this.numberOfCarsToBuyAtATime) {
-          return this._flush(callback);
-        }
-        callback();
-      });
-    }
-  }
-
-
-  class NewCarThief extends Transform<INewCar, IStolenCar> {
-    constructor() {
-      super({objectMode: true});
-    }
-    public _transform(chunk: INewCar, encoding: string, callback: (error?: (Error | null), data?: any) => void): void {
-      this.push({ stolenCarNumber: chunk.newCarNumber }, encoding);
-      callback();
-    }
-  }
-
-
   it('streams work', async () => {
 
     const carFactory = new NewCarFactory();
@@ -159,3 +22,138 @@ describe('stream', () => {
     console.log('done');
   });
 });
+
+
+interface INewCar {
+  newCarNumber: number;
+}
+
+interface IUsedCar {
+  usedCarNumber: number;
+}
+
+interface IStolenCar {
+  stolenCarNumber: number;
+}
+
+class NewCarFactory extends Readable<INewCar> {
+  private i = 0;
+
+  constructor() {
+    super({ objectMode: true });
+  }
+
+  public _read(): any {
+    process.nextTick(() => {
+      const v = this.i++ < 20 ? { newCarNumber: this.i } as INewCar : null;
+      this.push(v);
+      if (v) {
+        console.log('produced car# ', v.newCarNumber);
+      }
+    });
+  }
+}
+
+class NewCarBuyer extends Transform<INewCar, IUsedCar[]> {
+  private newCars: INewCar[] = [];
+
+  constructor(private readonly numberOfCarsToBuyAtATime: number) {
+    super({ objectMode: true });
+  }
+
+  public _flush(callback: (error?: (Error | null), data?: any) => void): void {
+    process.nextTick(() => {
+      if (this.newCars.length) {
+        this.push(this.newCars.map(m => ({ usedCarNumber: m.newCarNumber } as IUsedCar)));
+        console.log(`purchased ${this.newCars.length} car numbers: ${this.newCars.map(m => m.newCarNumber).join(',')}`);
+      }
+      this.newCars = [];
+      callback();
+    });
+  }
+
+  // tslint:disable-next-line:variable-name
+  public _transform(chunk: INewCar, _encoding: string, callback: (error?: (Error | null), data?: any) => void): void {
+    process.nextTick(() => {
+      this.newCars.push(chunk);
+      console.log('shopping for NEW car# ', chunk.newCarNumber);
+      if (this.newCars.length >= this.numberOfCarsToBuyAtATime) {
+        return this._flush(callback);
+      }
+      callback();
+    });
+  }
+}
+
+class UsedCarBuyer extends Transform<IUsedCar[], IUsedCar[]> {
+  private newCars: IUsedCar[] = [];
+
+  constructor(private readonly numberOfCarsToBuyAtATime: number) {
+    super({ objectMode: true });
+  }
+
+  public _flush(callback: (error?: (Error | null), data?: any) => void): void {
+    process.nextTick(() => {
+      if (this.newCars.length) {
+        this.push(this.newCars.map(m => ({ usedCarNumber: m.usedCarNumber } as IUsedCar)));
+        console.log(`purchased used ${this.newCars.length} car numbers: ${this.newCars.map(m => m.usedCarNumber).join(',')}`);
+      }
+      this.newCars = [];
+      callback();
+    });
+  }
+
+  // tslint:disable-next-line:variable-name
+  public _transform(chunk: IUsedCar[], _encoding: string, callback: (error?: (Error | null), data?: any) => void): void {
+    process.nextTick(() => {
+      chunk.forEach(c => this.newCars.push(c));
+      console.log('shopping for USED car# ', chunk.map(m => m.usedCarNumber).join(","));
+      if (this.newCars.length >= this.numberOfCarsToBuyAtATime) {
+        return this._flush(callback);
+      }
+      callback();
+    });
+  }
+}
+
+class StolenCarBuyer extends Transform<IStolenCar, IUsedCar[]> {
+  private newCars: IStolenCar[] = [];
+
+  constructor(private readonly numberOfCarsToBuyAtATime: number) {
+    super({ objectMode: true });
+  }
+
+  public _flush(callback: (error?: (Error | null), data?: any) => void): void {
+    process.nextTick(() => {
+      if (this.newCars.length) {
+        this.push(this.newCars.map(m => ({ usedCarNumber: m.stolenCarNumber } as IUsedCar)));
+        console.log(`purchased STOLEN ${this.newCars.length} car numbers: ${this.newCars.map(m => m.stolenCarNumber).join(',')}`);
+      }
+      this.newCars = [];
+      callback();
+    });
+  }
+
+  // tslint:disable-next-line:variable-name
+  public _transform(chunk: IStolenCar, _encoding: string, callback: (error?: (Error | null), data?: any) => void): void {
+    process.nextTick(() => {
+      this.newCars.push(chunk);
+      console.log('shopping for stolen car# ', chunk.stolenCarNumber);
+      if (this.newCars.length >= this.numberOfCarsToBuyAtATime) {
+        return this._flush(callback);
+      }
+      callback();
+    });
+  }
+}
+
+
+class NewCarThief extends Transform<INewCar, IStolenCar> {
+  constructor() {
+    super({objectMode: true});
+  }
+  public _transform(chunk: INewCar, encoding: string, callback: (error?: (Error | null), data?: any) => void): void {
+    this.push({ stolenCarNumber: chunk.newCarNumber }, encoding);
+    callback();
+  }
+}
