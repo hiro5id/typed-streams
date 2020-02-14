54e979133cc953d06659b6dbaa52b54c25dcb14d
Roberto Sebestyen <roberto@sebestyen.ca>
2020-02-14 00:55:58 -0500

    accept AggregationCursor in MongoToReadable

--- src/mongo-to-readable.ts
+++ src/mongo-to-readable.ts
@@ -1,8 +1,8 @@
-import { Cursor } from 'mongodb';
+import { AggregationCursor, Cursor } from 'mongodb';
 import { commonPipe } from './common-pipe';
 import { Readable } from './stream-readable-typed';
 
-export function MongoToReadable<T>(mongoStream: Cursor<T>, name: string, throwErrorForTesting = false): Readable<T> {
+export function MongoToReadable<T>(mongoStream: Cursor<T> | AggregationCursor<T>, name: string, throwErrorForTesting = false): Readable<T> {
   (mongoStream as any).name = name;
   (mongoStream as any).super = mongoStream.pipe;
   (mongoStream as any).pipe = function(destination: NodeJS.WritableStream, options?: { end?: boolean }) {
